41f71f1566bde07a2a68da11e4cfc55dee176f51
Srikar Tangallapally <63287284+srikar-amigo@users.noreply.github.com>
2023-06-28 23:00:34 +0530

    Added files from mac desktop

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = (Math.floor(Math.random()*6)) + 1;
+
+var randomImageSource = "images/dice" + randomNumber1 +".png" ;
+
+console.log(randomImageSource)
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = (Math.floor(Math.random()*6)) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 +".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = " Play 2 Wins! 🚩";
+}else{
+    document.queryselector("h1").innerHTML = "DRAW!";
+}
